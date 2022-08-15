96860fa2bf3cdd34aa2efd412dbc5316aa57ff95
Anders Pettersen <anders.pettersen@gmail.com>
2022-08-15 22:46:01 +0200

    Mostly sorted. Stopping for the night.

--- RPS.js
+++ RPS.js
@@ -1,5 +1,24 @@
 function rockpaperscissors() {
-    console.log("lol");
+    let option = prompt("What's your weapon of choice?");
+    let opNum = 0;
+    let randInt = Math.floor(Math.random() * 3);
+    console.log(randInt);
+    
+    if (option == "rock") {
+        opNum = 0;
+    } else if (option == "paper") {
+        opNum = 1;
+    } else if (option == "scissors") {
+        opNum = 2;
+    }
+    console.log(opNum);
+
+    // now 2 beats 1 beats 0 beats 2
+
+    switch (opnum) {
+        
+    }
+
 }
 
 rockpaperscissors();
